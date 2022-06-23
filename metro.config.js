33cbeeec30acd86980ab79137fcbf612ffa8ce10
Buddha Lama <lama@Buddhas-MacBook-Pro.local>
2022-06-23 10:17:34 +0545

    docs: ✏️ initial commit

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const blacklist = require('metro-config/src/defaults/exclusionList');
+
+module.exports = {
+  resolver: {
+    blacklistRE: blacklist([
+      /ios\/Pods\/JitsiMeetSDK\/Frameworks\/JitsiMeet.framework\/assets\/node_modules\/react-native\/.*/,
+    ]),
+  },
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+};
